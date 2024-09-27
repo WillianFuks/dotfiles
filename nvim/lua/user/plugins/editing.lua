a2af790386170cf043aaba2468f1591628fa0e8c
Willian Fuks <willian.fuks@gmail.com>
2024-09-27 12:26:31 -0300

    remove mini comments (now it is builtin on nvim0.10)

--- nvim/lua/user/plugins/editing.lua
+++ nvim/lua/user/plugins/editing.lua
@@ -1,36 +1,10 @@
 return {
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
+    enabled = false,
     lazy = true,
     opts = {
       enable_autocmd = false,
-    },
-    dependencies = { "echasnovski/mini.comment" },
-  },
-  {
-    "echasnovski/mini.comment",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-      mappings = {
-        -- Toggle comment (like `gcip` - comment inner paragraph) for both
-        -- Normal and Visual modes
-        comment = 'gc',
-
-        -- Toggle comment on current line
-        comment_line = '<leader>c',
-
-        -- Toggle comment on visual selection
-        comment_visual = '<leader>c',
-
-        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-        -- Works also in Visual mode if mapping differs from `comment_visual`
-        textobject = 'gc',
-      }
     },
   },
   {
