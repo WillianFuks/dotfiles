f0e21ef95acdd551f0310b910bad7509a94d7d20
WillianFuks <willian.fuks@gmail.com>
2026-02-19 14:46:30 -0300

    update telescope tag

--- nvim/.config/nvim/lua/user/plugins/navigation.lua
+++ nvim/.config/nvim/lua/user/plugins/navigation.lua
@@ -131,7 +131,7 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
